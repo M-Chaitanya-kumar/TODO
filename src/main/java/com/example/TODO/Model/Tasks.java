96fc4713901829b352cfb50cf989bc86d8f04eee
= <=>
2025-07-26 12:25:27 +0530

    First commit

---
+++ src/main/java/com/example/TODO/Model/Tasks.java
@@ -0,0 +1,5 @@
+package com.example.TODO.Model;
+
+public class Tasks {
+    
+}
---
+++ src/main/resources/Static/index.js
@@ -0,0 +1,58 @@
+function addTaskToList(task)
+{
+    const li = document.createElement('li');
+    li.textContent = task.title;
+    document.getElementsById('task-list').appendChild(li);
+}
+
+async function addTaskToBanckend(task)
+{
+    const response = await fetch("http://localhost:8080/tasks",
+        {
+            method: "POST",
+            headers:
+            {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(task)
+        }
+    );
+
+    if (response.ok)
+    {
+        const result = await response.json();
+        
+        addTaskToList(result);
+        
+        document.getElementById('task-title').value = "";
+    }
+    else
+    {
+        alert("failed to add the task");
+    }
+}
+
+
+document.getElementById("task-form").addEventListener("submit", 
+    async function (e)
+{
+    e.preventDefault();
+
+    const title = document.getElementById("task-title");
+
+    const task =
+    {
+        title           : title,
+        description     : "",
+        status          : "pending"
+    };
+
+    try{
+        await addTaskToBanckend(task);
+    }
+    catch(error)
+    {
+        console.log("Error connecting to backend",error);
+        alert("Error in connecting to Backend");
+    }
+})
